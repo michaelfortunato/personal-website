1cc463edd127cd5f780624ff2f0830242690dbbf
Michael Fortunato <michael.n.fortunato@gmail.com>
2021-09-19 18:00:45 -0400

    removed eslint error

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/display-name */
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets as MaterialUIServerStyleSheets } from "@material-ui/core/styles";
