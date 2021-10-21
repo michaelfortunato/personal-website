bfc4c3d2e84f3259ce29abf9bb549b8d348e0d98
Michael Fortunato <michael.n.fortunato@gmail.com>
2021-10-21 03:05:07 -0400

    cool

--- pages/blog/[bid].js
+++ pages/blog/[bid].js
@@ -1,40 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import Grid from '@components/Grid';
-import Hero from '@components/Hero';
-import { AnimatePresence, motion } from 'framer-motion'
-import fs from "fs"
-import Typography from '@material-ui/core/Typography';
-import ReactMarkdown from 'react-markdown'
-import rehypeKatex from 'rehype-katex'
-import remarkMath from 'remark-math'
-import 'katex/dist/katex.min.css'
-import path from 'path';
+import React, { useState, useEffect } from "react";
+import Grid from "@components/Grid";
+import Hero from "@components/Hero";
+import { AnimatePresence, motion } from "framer-motion";
+import Typography from "@material-ui/core/Typography";
+import ReactMarkdown from "react-markdown";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import "katex/dist/katex.min.css";
+import path from "path";
+import axios from "axios";
+import parser from "fast-xml-parser";
+import { assetsURL } from "@utils/configurations";
 
-const blogsDir = process.cwd() + path.sep + "_blogs"
 export default function Blog(props) {
-    return <div>
-        <Typography variant="h2">{props.bid}</Typography>
-        <ReactMarkdown
-            remarkPlugins={[remarkMath]}
-            rehypePlugins={[rehypeKatex]}
-        >{props.content}
-        </ReactMarkdown>
+  return (
+    <div style={{ backgroundColor: "white" }}>
+      <ReactMarkdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+        {props.content}
+      </ReactMarkdown>
     </div>
+  );
 }
 
 export async function getStaticProps({ params }) {
-    const content = fs.readFileSync(blogsDir + path.sep + params.bid, "utf-8");
-    return {
-        props: {
-            bid: params.bid,
-            content: content
-        }
-    }
+  const { data } = await axios.get(`${assetsURL}/blogs/${params.bid}.md`);
+
+  return {
+    props: {
+      bid: params.bid,
+      content: data,
+    },
+  };
 }
 export async function getStaticPaths() {
-    const paths = fs.readdirSync(blogsDir).map(file => ({ params: { bid: file } }));
-    return {
-        paths: paths,
-        fallback: false // 'blocking' // See the "fallback" section below
-    };
+  const { data } = await axios.get(`${assetsURL}/`, {
+    params: {
+      "list-type": 2,
+      prefix: "blogs/",
+      delimiter: "/",
+      Bucket: "assets.michaelfortunato.org",
+    },
+  });
+  let {
+    ListBucketResult: { Contents },
+  } = parser.parse(data);
+  if (!Array.isArray(Contents)) {
+    Contents = [Contents];
+  }
+  const paths = Contents.map(({ Key }) => ({
+    params: {
+      bid: Key.split("/").pop().replace(".md", ""),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
---
+++ utils/configurations.js
@@ -0,0 +1,3 @@
+// This file will house some universal configurations used by the web app
+const assetsURL = "https://assets.michaelfortunato.org";
+export { assetsURL };
