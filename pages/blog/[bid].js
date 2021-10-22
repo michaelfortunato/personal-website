5a73c076669dca20a66f1aaf35862fd7bf68c2b2
Michael Fortunato <michael.n.fortunato@gmail.com>
2021-10-22 01:55:55 -0400

    nice ok

--- pages/blog/[bid].js
+++ pages/blog/[bid].js
@@ -7,6 +7,7 @@ import ReactMarkdown from "react-markdown";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 import "katex/dist/katex.min.css";
+import fs from "fs";
 import path from "path";
 import axios from "axios";
 import parser from "fast-xml-parser";
@@ -23,38 +24,67 @@ export default function Blog(props) {
 }
 
 export async function getStaticProps({ params }) {
-  const { data } = await axios.get(`${assetsURL}/blogs/${params.bid}.md`);
+  if (process.env.NODE_ENV !== "development") {
+    const { data } = await axios.get(`${assetsURL}/blogs/${params.bid}.md`);
 
-  return {
-    props: {
-      bid: params.bid,
-      content: data,
-    },
-  };
+    return {
+      props: {
+        bid: params.bid,
+        content: data,
+      },
+    };
+  } else {
+    const blogPath = path.join(
+      process.cwd(),
+      "..",
+      assetsURL,
+      `${params.bid}.md`
+    );
+    const data = fs.readFileSync(blogPath, "utf-8");
+
+    return {
+      props: {
+        bid: params.bid,
+        content: data,
+      },
+    };
+  }
 }
 export async function getStaticPaths() {
-  const { data } = await axios.get(`${assetsURL}/`, {
-    params: {
-      "list-type": 2,
-      prefix: "blogs/",
-      delimiter: "/",
-      Bucket: "assets.michaelfortunato.org",
-    },
-  });
-  let {
-    ListBucketResult: { Contents },
-  } = parser.parse(data);
-  if (!Array.isArray(Contents)) {
-    Contents = [Contents];
-  }
-  const paths = Contents.map(({ Key }) => ({
-    params: {
-      bid: Key.split("/").pop().replace(".md", ""),
-    },
-  }));
+  if (process.env.NODE_ENV !== "development") {
+    const { data } = await axios.get(`${assetsURL}/`, {
+      params: {
+        "list-type": 2,
+        prefix: "blogs/",
+        delimiter: "/",
+        Bucket: "assets.michaelfortunato.org",
+      },
+    });
+    let {
+      ListBucketResult: { Contents },
+    } = parser.parse(data);
+    if (!Array.isArray(Contents)) {
+      Contents = [Contents];
+    }
+    const paths = Contents.map(({ Key }) => ({
+      params: {
+        bid: Key.split("/").pop().replace(".md", ""),
+      },
+    }));
 
-  return {
-    paths,
-    fallback: false,
-  };
+    return {
+      paths,
+      fallback: false,
+    };
+  } else {
+    const localBlogsFodler = path.join(process.cwd(), "..", assetsURL);
+    const blogs = fs.readdirSync(localBlogsFodler);
+    const paths = blogs.map((blog) => ({
+      params: { bid: blog.replace(".md", "") },
+    }));
+    return {
+      paths,
+      fallback: false,
+    };
+  }
 }
--- utils/configurations.js
+++ utils/configurations.js
@@ -1,3 +1,7 @@
+import path from "path";
 // This file will house some universal configurations used by the web app
-const assetsURL = "https://assets.michaelfortunato.org";
+const assetsURL =
+  process.env.NODE_ENV !== "development"
+    ? "https://assets.michaelfortunato.org"
+    : path.join("assets.michaelfortunato.org", "blogs");
 export { assetsURL };
