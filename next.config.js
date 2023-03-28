9ec3dacfe04837621d852b015265a1ec55b28a2a
Michael Fortunato <michael.n.fortunato@gmail.com>
2023-03-28 18:30:33 -0400

    trying this to get amplify builds to work

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
+  output: "standalone",
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
