3c7e888e92e836fe46118ab51149fd89be0674e1
Michael Fortunato <michael.n.fortunato@gmail.com>
2023-03-28 18:35:02 -0400

    undoing that removal

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
-  output: "standalone",
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
