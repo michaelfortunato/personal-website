aad3a06ff1408eeb98013662a50dc317831a8eb3
Michael Fortunato <michael.n.fortunato@gmail.com>
2023-03-28 14:47:52 -0400

    projects page

--- components/Projects/Tile.tsx
+++ components/Projects/Tile.tsx
@@ -1,1 +1,6 @@
 import React from "react";
+import { Paper } from "@material-ui/core";
+
+export default function Tile() {
+  return <Paper>Bing Bong</Paper>;
+}
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,10 +1,33 @@
 import React from "react";
+import Tile from "@components/Projects/Tile";
+import { Grid, Paper, Typography } from "@material-ui/core";
+import Image from "next/image";
 
 export default function Projects(props: any) {
-  console.log(props);
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
-      <h2>Here</h2>
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid component={Paper} container item xs={9}>
+          <Grid item xs={6}>
+            <Typography variant="h1" style={{ color: "black" }}>
+              Personal Website
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Image
+              src="/projects/clay-iphone.svg"
+              alt="placertexthere"
+              width={50}
+              height={50}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
