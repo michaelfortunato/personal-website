b7507c3de2bbfa80f4bddafa4151dd55ec8ef3ef
Michael Fortunato <michael.n.fortunato@gmail.com>
2023-08-02 11:59:53 -0400

    Upgraded javascript dependencies. Everything is broken. This is probably a solid 48h of ui work to fix. WORTH IT.

---
+++ pages/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import Grid from "@components/Grid";
+import Hero from "@components/Hero";
+import { AnimatePresence, motion } from "framer-motion";
+
+const defaultGridConfig = {
+	random: true,
+	numLines: 12,
+	offset: 0,
+	avgDuration: 150,
+	avgDelay: 1500,
+	duration: 750,
+
+	isDot: true
+};
+
+export default function Home(props: any) {
+	const [gridEntered, setGridEntered] = useState(false);
+	const [triggerNameEnter, setTriggerNameEnter] = useState(false);
+	const [triggerGridExit, setTriggerGridExit] = useState(false);
+	useEffect(() => {
+		document.body.overflowY = "hidden";
+	}, []);
+	console.log("I GOT RENDERED");
+	return (
+		<motion.div
+			style={{ overflow: "hidden", height: "100vh", width: "100vw" }}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1, transition: { duration: 0.3 } }}
+			exit={{ opacity: 0, transition: { delay: 0.6, duration: 0.3 } }}
+		>
+			<AnimatePresence>
+				{!triggerGridExit && (
+					<motion.div exit={{ opacity: 0 }}>
+						<Grid
+							setTriggerGridExit={setTriggerGridExit}
+							setTriggerNameEnter={setTriggerNameEnter}
+							{...defaultGridConfig}
+						/>
+					</motion.div>
+				)}
+			</AnimatePresence>
+			<Hero gridEntered={gridEntered} triggerNameEnter={triggerNameEnter} />
+		</motion.div>
+	);
+}
