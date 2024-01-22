d954ca79f143ed12330dce1827cf66ea8d6d1a6b
Michael Fortunato <michael.n.fortunato@gmail.com>
2024-01-21 21:21:47 -0500

    straight to main

---
+++ components/layout.tsx
@@ -0,0 +1,14 @@
+type Props = {
+	preview?: boolean;
+	children: React.ReactNode;
+};
+
+const Layout = ({ preview, children }: Props) => {
+	return (
+		<div className="min-h-screen">
+			<main>{children}</main>
+		</div>
+	);
+};
+
+export default Layout;
