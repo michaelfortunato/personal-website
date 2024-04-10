177884fd651bfdcfaaf58793f2c0f7b7607541e1
Michael Fortunato <michael.n.fortunato@gmail.com>
2024-04-09 23:53:45 -0400

    ci: normalize build env vars and harden test script

--- scripts/test-deploy.sh
+++ scripts/test-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-act --workflows "$(git rev-parse --git-path ../.github/workflows)" \
+cd "$(git rev-parse --git-path ../)"
+echo "Testing ONLY .github/workflows/deploy-dev.yaml ..."
+act --workflows "$(git rev-parse --git-path ../.github/workflows/deploy-dev.yaml)" \
 	--secret-file "$(git rev-parse --git-path ../.env.local)"
