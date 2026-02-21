99ffc82868934b33813ec8ee3838841050b50972
Michael Fortunato <michael.n.fortunato@gmail.com>
2026-02-20 22:40:52 -0600

    Add rudimentary dropdown modal for mobile

--- components/Blog/layout.tsx
+++ components/Blog/layout.tsx
@@ -3,21 +3,76 @@ import Link from "next/link";
 import { ThemeSwitch } from "@/components/ui/ThemeSwitch";
 import { BuildInfo, SerializedBuildInfo } from "@/lib/buildInfo";
 import BuildStamp from "@/components/BuildStamp";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "next-themes";
 type Props = {
   children: React.ReactNode;
   websiteWideBuildInfo?: SerializedBuildInfo;
 };
 
 function Header() {
+  const { theme, setTheme } = useTheme();
   return (
     <div className={`px-16 md:flex md:items-center md:justify-between `}>
       <div className="flex justify-center">
         <Link
           href="/blog"
-          className={`bold ${blogInitialsFont.className} text-4xl relative inline-block after:content-['.'] after:absolute after:left-full`}
+          className={`hidden md:inline-block bold ${blogInitialsFont.className} text-4xl relative inline-block after:content-['.'] after:absolute after:left-full`}
         >
           mnf
         </Link>
+        <DropdownMenu>
+          <DropdownMenuTrigger
+            className={`md:hidden bold ${blogInitialsFont.className} text-4xl relative inline-block after:content-['.'] after:absolute after:left-full`}
+            asChild
+          >
+            <button>mnf</button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuGroup>
+              <DropdownMenuItem asChild>
+                <Link href="/blog/on-work">On Work</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/blog/on-quiet">On Quiet</Link>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuRadioGroup
+              onValueChange={(value) => setTheme(value)}
+              value={theme}
+              className="flex"
+            >
+              <DropdownMenuRadioItem
+                onSelect={(e) => e.preventDefault()}
+                value="light"
+              >
+                Light
+              </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem
+                onSelect={(e) => e.preventDefault()}
+                value="dark"
+              >
+                Dark
+              </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem
+                onSelect={(e) => e.preventDefault()}
+                value="system"
+              >
+                System
+              </DropdownMenuRadioItem>
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
       <div className="hidden md:flex md:gap-4">
         <Link href="/blog/on-work">On Work</Link>
